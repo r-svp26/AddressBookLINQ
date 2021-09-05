4346ac173f15f9b9deb5147949e0e65fe9af8b86
r-svp26 <riteshk6341@gmail.com>
2021-09-05 19:50:44 +0530

    [Ritesh] Add: Welcome to Address Book LINQ Program

---
+++ AddressBookLINQ/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AddressBookLINQ
+{
+    class Program
+    {
+        /// <summary>
+        /// main method
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book LINQ Program");
+        }
+    }
+}
